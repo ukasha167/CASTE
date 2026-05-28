ca8a6e8fae87c16c5004bec10aab5f7f7a044ca1
Ukasha Anwar <159612886+ukasha167@users.noreply.github.com>
2026-05-28 20:13:48 +0500

    UPDATE: Set the montserrat font as default

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'screens/home_screen.dart';
 
 void main() {
@@ -14,6 +15,9 @@ class MyApp extends StatelessWidget {
       title: "Weather",
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
+        textTheme: GoogleFonts.montserratTextTheme(
+          Theme.of(context).textTheme,
+        ),
         scaffoldBackgroundColor: const Color.fromARGB(255, 6, 68, 225),
       ),
       home: const HomeScreen(),
